0a75b2c2d9594b1e37196e26b1554e8936834a03
박기현 <kihyun1998@naver.com>
2024-01-25 08:52:38 +0900

    상단바 커스텀

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:todo_application/util/lang/generated/l10n.dart';
 
 void main() {
   runApp(const MyApp());
+  doWhenWindowReady(() {
+    final win = appWindow;
+    win.minSize = const Size(600, 450);
+    win.size = const Size(1200, 600);
+    win.alignment = Alignment.center;
+    win.title = "TodoApp";
+    win.show();
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -20,7 +29,7 @@ class MyApp extends StatelessWidget {
       ],
       supportedLocales: S.delegate.supportedLocales,
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'TodoApp',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
